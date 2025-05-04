86bd938b0fe11ad4977dfa35e5c020b6d8bf1e12
Ferdinand Niyongire <ferdinandniyongira@gmail.com>
2025-05-03 19:38:36 -0500

    Second commit!

--- ForLoop.java
+++ ForLoop.java
@@ -1,7 +1,7 @@
 public class ForLoop {
     public static void main(String[] args) {
-        for(int x = 0; x<=10; x++) {
-            System.out.println(x +" There we go " + x );
+        for(int x = 0; x<=10; x++) {           //initialization, condition, and update statement
+            System.out.println(x +" There we go " + x );      //Loop body
         }
     }
 }
---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+
+        String name = "Jay";
+
+        String name2 = new String("JD");
+
+        System.out.println(name);
+        System.out.println(name2);
+    }
+}
